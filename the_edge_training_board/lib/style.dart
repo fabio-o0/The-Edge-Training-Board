3a86457e7083a1376e637b87b7658a7e2f80c07a
Fabio Suarez <fabiosuarez.o0@gmail.com>
2021-03-23 05:02:20 -0400

    Added Assets

---
+++ the_edge_training_board/lib/style.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static final ThemeData theme = ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Color(0xffd6ae4e),
+      backgroundColor: Colors.white,
+      errorColor: Color(0xffED2939),
+
+      fontFamily: 'Overpass',
+
+      textTheme: TextTheme(
+        headline1: TextStyle(fontSize: 50.50, fontWeight: FontWeight.w300, color: Colors.black),
+        headline2: TextStyle(fontSize: 37.89, fontWeight: FontWeight.w500, color: Colors.black),
+        headline3: TextStyle(fontSize: 28.42, fontWeight: FontWeight.w500, color: Colors.black),
+        headline4: TextStyle(fontSize: 21.32, fontWeight: FontWeight.w500, color: Colors.black),
+        headline5: TextStyle(fontSize: 16.00, fontWeight: FontWeight.w500, color: Colors.black),
+        headline6: TextStyle(fontSize: 12.00, fontWeight: FontWeight.w700, color: Colors.black),
+        subtitle1: TextStyle(fontSize: 12.00, fontWeight: FontWeight.w500, color: Colors.black),
+        subtitle2: TextStyle(fontSize: 9.00, fontWeight: FontWeight.w700, color: Colors.black),
+        bodyText1: TextStyle(fontSize: 12.00, fontWeight: FontWeight.w400, color: Colors.black),
+        bodyText2: TextStyle(fontSize: 9.00, fontWeight: FontWeight.w500, color: Colors.black),
+        button: TextStyle(fontSize: 21.32, fontWeight: FontWeight.w700, color: Colors.black),
+        overline: TextStyle(fontSize: 16.00, fontWeight: FontWeight.w900, color: Color(0xffd6ae4e)),
+      ),
+  );
+}
